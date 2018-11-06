8950ffe8efa59782e8a3a77603a0849e1803ca11
AstroMod <43607175+AstroMod@users.noreply.github.com>
2018-11-06 03:57:36 +0000

    Update record keeping app.cpp

--- record keeping app.cpp
+++ record keeping app.cpp
@@ -1,88 +1,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 int id = 1;
 
 struct Student {
-    std::string first_name;
-    std::string surname;
+    std::string first_name = "unkown";
+    std::string surname = "unknown";
     int s_id = id;
 
-    void print (const Student& s) const {
-        std::cout << s.s_id << " - " << s.first_name << " " << s.surname;
+    void print () const {
+        std::cout << s_id << " - " << first_name << " " << surname;
     }
 };
-
-std::vector<Student> list_of_student_in_uni = {};
-
-bool check_new (std::string s) {
-    bool x = 1;
-    for (Student member : list_of_student_in_uni) {
-        if (member.surname == s) {
-            x = 0;
-        }
-        else continue;
-    }
-    return x;
-}
-
-void add_student(std::string fn, std::string s) {
-    if (check_new(s)) {
-        Student st = {fn, s};
-        list_of_student_in_uni.push_back(st);
-        id += 1;
-    } else {
-        std::cout << "Student exists already! " << std::endl;
-    }
-}
-
-void print (std::vector<Student> list) {
-    for (Student member : list) {
-        member.print(member);
-        std::cout << std::endl;
-    }
-}
 
 struct ModuleRecord {
-    int id;
+    Student my_student;
     int grade = 0;
 };
+
+bool sortByGrade(const ModuleRecord &lhs, const ModuleRecord &rhs) {
+    return lhs.grade > rhs.grade;
+}
 
 struct Module {
     std::string title;
     std::vector<ModuleRecord> list_of_grades_per_student = {};
 
-    void add_record (int id, int grade) {
-        ModuleRecord pair_student_grade = {id, grade};
+    void add_record (const Student& name, const int& grade) {
+        ModuleRecord pair_student_grade = {name, grade};
         list_of_grades_per_student.push_back(pair_student_grade);
+        id++;
     }
 
     void print (const Module& m) {
         std::cout << m.title << std::endl;
+        std::sort(list_of_grades_per_student.begin(), list_of_grades_per_student.end(), sortByGrade);
         for (ModuleRecord element : list_of_grades_per_student) {
-            element.my_student.print(element.my_student);
+            element.my_student.print();
             std::cout << " " << element.grade << std::endl;
         }
     }
 };
 
+
+
 int main() {
-    Uni u = {"CPP London Uni"};
-    std::string fn = "Barbara";
-    std::string s = "L'Episcopo";
-    u.add_student(fn, s);
-    u.print(u);
-    std::cout << std::endl;
-    std::string fn2 = "Fabio";
-    std::string s2 = "Platania";
-    u.add_student(fn2, s2);
-    u.print(u);
-    std::cout << std::endl;
-    std::string fn3 = "Giulia";
-    std::string s3 = "Platania";
-    u.add_student(fn3, s3);
-    u.print(u);
+
+    Module m1 {"ICT"};
+    m1.add_record ({"Mickey", "Mouse"}, 9);
+    m1.add_record({"Donald", "Duck"}, 7);
+    m1.add_record({"Minnie", "Mouse"}, 7);
+    m1.add_record({"Uncle", "Goofy"}, 8);
+    m1.print(m1);
+
+    Module m2 {"Maths"};
+    m2.add_record ({"Donald", "Trump"}, 9);
+    m2.add_record({"George W.", "Bush"}, 7);
+    m2.add_record({"Bill", "Clinton"}, 5);
+    m2.add_record({"Richard", "Nixon"}, 5);
+    m2.print(m2);
+
 
     return 0;
 }
