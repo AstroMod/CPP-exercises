b5ef511cbe9f874fde8d2941408a003d9d12fef7
AstroMod <43607175+AstroMod@users.noreply.github.com>
2018-11-12 01:39:02 +0000

    Add files via upload

--- conversion.cpp
+++ conversion.cpp
@@ -1,40 +1,40 @@
 #include "conversion.hpp"
 #include <sstream>
 
-void Metres::operator+= (Feet toAdd_f) {
+void Metres::operator+= (const Feet toAdd_f) {
     m += to_metres(toAdd_f).m;
 }
 
-void Metres::operator+= (Metres toAdd_m) {
+void Metres::operator+= (const Metres toAdd_m) {
     m += toAdd_m.m;
 }
 
-void Feet::operator+= (Feet toAdd_f) {
+void Feet::operator+= (const Feet toAdd_f) {
     f += toAdd_f.f;
 }
 
-void Feet::operator+= (Metres toAdd_m) {
+void Feet::operator+= (const Metres toAdd_m) {
     f += to_feet(toAdd_m).f;
 }
 
-Feet to_feet (Metres actual_m) {
+Feet to_feet (const Metres actual_m) {
     Feet new_f {actual_m.m/0.3048};
     return new_f;
 }
 
-Metres to_metres (Feet actual_f) {
+Metres to_metres (const Feet actual_f) {
     Metres new_m {actual_f.f*0.3048};
     return new_m;
 }
 
-std::string to_string(Metres conv_m) {
+std::string to_string(const Metres conv_m) {
     std::ostringstream temp;
     temp << conv_m.m;
     std::string st = temp.str();
     return st;
 }
 
-std::string to_string(Feet conv_f) {
+std::string to_string(const Feet conv_f) {
     std::ostringstream temp;
     temp << conv_f.f;
     std::string st = temp.str();
--- conversion.hpp
+++ conversion.hpp
@@ -6,20 +6,20 @@ typedef struct Metres Metres;
 struct Metres {
     double m = 0;
 
-    void operator+= (Metres toAdd_m);
-    void operator+= (Feet toAdd_f);
+    void operator+= (const Metres toAdd_m);
+    void operator+= (const Feet toAdd_f);
 };
 
 struct Feet {
     double f = 0;
 
-    void operator+= (Feet toAdd_f);
-    void operator+= (Metres toAdd_m);
+    void operator+= (const Feet toAdd_f);
+    void operator+= (const Metres toAdd_m);
 };
 
-Feet to_feet (Metres actual_m);
-Metres to_metres (Feet actual_f);
+Feet to_feet (const Metres actual_m);
+Metres to_metres (const Feet actual_f);
 
-std::string to_string(Metres m);
-std::string to_string(Feet f);
+std::string to_string(const Metres m);
+std::string to_string(const Feet f);
 
